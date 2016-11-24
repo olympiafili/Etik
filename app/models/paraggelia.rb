c418c0eaf3b1674ac79f40fc3ae396e4b9ee9268
olympiafili <olympia.filippa@gmail.com>
2016-11-24 17:32:43 +0200

    Etik
    changes in csv

--- app/models/paraggelia.rb
+++ app/models/paraggelia.rb
@@ -20,7 +20,7 @@ class Paraggelia < ActiveRecord::Base
 	  end
 	end
   
-	def to_csv(options = {:col_sep => ";"})
+	def to_csv(options = {:col_sep => ";", :row_sep => "\r\n"})
     CSV.generate(options) do |csv|
       @customer = SimpleUserPse.where(:id => self.customer).first
       csv << ["DEALER", self.user]
@@ -35,30 +35,36 @@ class Paraggelia < ActiveRecord::Base
         @open_type = OpenType.where(:name => order.open_type_id).first
         
         #ELEPOS
-        csv << ["ELEPOS", order.id, order.posotoita, @open_type.code, @open_type.code, order.main_color_id, order.open_type_id, 
+        @main_color = Color.where(:name => order.main_color_id).first
+        csv << ["ELEPOS", order.id, order.posotoita, @open_type.code, @open_type.code, @main_color.sungate_both, order.open_type_id, 
           order.width, order.height, order.width_new, order.height_new, @line.sungate_code, order.tzamia0, "<Handle inside>", "<Handle outside>", 
           "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.price_sum]
         
         #ADDOPTPOS
         #roll guides
         if (order.odoigos != nil)
-          csv << ["ADDOPTPOS", order.odoigos, "Left/Right", order.up_odigou]
+          @roll_guide = RollGuide.where(:name => order.odoigos).first
+          csv << ["ADDOPTPOS", @roll_guide.sungate_code, "Left/Right", order.up_odigou]
         end
         #persida
         if (order.persida != nil)
-          csv << ["ADDOPTPOS", order.persida, "Top", order.pl_persidas]
+           @persida = Persides.where(:name => order.persida).first
+          csv << ["ADDOPTPOS", @persida.sungate_code, "Top", order.pl_persidas]
         end
         #prostasia
         if (order.prostasia_name != nil)
-          csv << ["ADDOPTPOS", order.prostasia_name, "<Location>", order.tm_p_prostasia]
+          @prostasia = TypoiProstasia.where(:name => order.prostasia_name).first
+          csv << ["ADDOPTPOS", @prostasia.sungate_code, "<Location>", order.tm_p_prostasia]
         end
         #rolo epik
         if (order.rolo != nil && order.timi_m_epik != nil)
-          csv << ["ADDOPTPOS", order.rolo, "Top", order.pl_rolou_ep]
+          @rolo_epik = RolaEpik.where(:name => order.rolo).first
+          csv << ["ADDOPTPOS", @rolo_epik.sungate_code, "Top", order.pl_rolou_ep]
         end
         #rolo eks
         if (order.rolo != nil && order.timi_m_ekso != nil)
-          csv << ["ADDOPTPOS", order.rolo, "Top", order.pl_rolou_ek]
+          @rolo_eks = RolaEkso.where(:name => order.rolo).first
+          csv << ["ADDOPTPOS", @rolo_eks.sungate_code, "Top", order.pl_rolou_ek]
         end
         #profil typos
         if (order.typos_katw_1 != nil)
@@ -115,75 +121,82 @@ class Paraggelia < ActiveRecord::Base
         end
         
         #ARTPOS
-        main_index = 1
-        main_index = main_index.to_s
-        index = 1
+        main_index = "1"
+        index = 10
         #roll guides
         if (order.odoigos != nil)
-          csv << ["ARTPOS", main_index, main_index + "." + index.to_s, 1, "m", order.odoigos, order.color_odoigou, 
+          @odoigos_color = Color.where(:name => order.color_odoigou).first
+          csv << ["ARTPOS", main_index, index.to_s, 1, "m", @roll_guide.sungate_code, @odoigos_color.sungate_both, 
             order.odoigos, 0, order.up_odigou, "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.timi_m_odoigou, "no"]
-            index +=1
+            index +=10
         end
         #persida
         if (order.persida != nil)
-          csv << ["ARTPOS", main_index, main_index + "." + index.to_s, 1, "m2", order.persida, order.color_persidas, 
+          @persida_color = Color.where(:name => order.color_persidas).first
+          csv << ["ARTPOS", main_index, index.to_s, 1, "m2", @persida.sungate_code, @persida_color.sungate_both, 
             order.persida, order.pl_persidas, order.up_persidas, "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.timi_m_persidas, "no"]
-            index +=1
+            index +=10
         end
         #prostasia
         if (order.prostasia_name != nil)
-          csv << ["ARTPOS", main_index, main_index + "." + index.to_s, 1, "m", order.prostasia_name, nil, 
+          csv << ["ARTPOS", main_index, index.to_s, 1, "m", @prostasia.sungate_code, nil, 
             order.prostasia_name, order.tm_p_prostasia, 0, "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.timi_m_p_prostasia, "no"]
-          index +=1
-        end
-        #rolo eks
-        if (order.rolo != nil && order.timi_m_ekso != nil)
-          csv << ["ARTPOS", main_index, main_index + "." + index.to_s, 1, "m2", order.rolo, order.color_rolou, 
-            order.rolo, order.pl_rolou_ek, order.up_rolou_ek, "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.timi_m_ekso, "no"]
-          index +=1
+          index +=10
         end
         #rolo epik
         if (order.rolo != nil && order.timi_m_epik != nil)
-          csv << ["ARTPOS", main_index, main_index + "." + index.to_s, 1, "m2", order.rolo, order.color_rolou, 
+          @rolo_color = Color.where(:name => order.color_rolou).first
+          csv << ["ARTPOS", main_index, index.to_s, 1, "m2", @rolo_epik.sungate_code, @rolo_color.sungate_both, 
             order.rolo, order.pl_rolou_ep, order.up_rolou_ep, "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.timi_m_epik, "no"]
-          index +=1
+          index +=10
+        end
+        #rolo eks
+        if (order.rolo != nil && order.timi_m_ekso != nil)
+          @rolo_color = Color.where(:name => order.color_rolou).first
+          csv << ["ARTPOS", main_index, index.to_s, 1, "m2", @rolo_eks.sungate_code, @rolo_color.sungate_both, 
+            order.rolo, order.pl_rolou_ek, order.up_rolou_ek, "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.timi_m_ekso, "no"]
+          index +=10
         end
         #profil typos
         if (order.typos_katw_1 != nil)
-          csv << ["ARTPOS", main_index, main_index + "." + index.to_s, 1, "m", order.typos_code, nil, 
+          csv << ["ARTPOS", main_index, index.to_s, 1, "m", order.typos_code, nil, 
             order.typos_katw_1, order.tm_p_t1, order.tm_u_t1, "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.timi_m_t_1, "no"]
-          index +=1
+          index +=10
         end
         #profil deksia
         for i in 1..3
           if (order.read_attribute("pd#{i}_code") != nil)
-            csv << ["ARTPOS", main_index, main_index + "." + index.to_s, order.read_attribute("profil_deksia_arithmos_#{i}"), "m", order.read_attribute("pd#{i}_code"), order.color_profil_d, 
+            @prd_color = Color.where(:name => order.color_profil_d).first
+            csv << ["ARTPOS", main_index, index.to_s, order.read_attribute("profil_deksia_arithmos_#{i}"), "m", order.read_attribute("pd#{i}_code"), @prd_color.sungate_both, 
               order.read_attribute("profil_deksia_#{i}"), order.read_attribute("tm_p_pd#{i}"), order.read_attribute("tm_u_pd#{i}"), "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.read_attribute("timi_m_p_d_#{i}"), "no"]
-            index +=1
+            index +=10
           end
         end
         #profil aristera
         for i in 1..3
           if (order.read_attribute("pa#{i}_code") != nil)
-            csv << ["ARTPOS", main_index, main_index + "." + index.to_s, order.read_attribute("profil_aristera_arithmos_#{i}"), "m", order.read_attribute("pa#{i}_code"), order.color_profil_a, 
+            @pra_color = Color.where(:name => order.color_profil_a).first
+            csv << ["ARTPOS", main_index, index.to_s, order.read_attribute("profil_aristera_arithmos_#{i}"), "m", order.read_attribute("pa#{i}_code"), @pra_color.sungate_both, 
               order.read_attribute("profil_aristera_#{i}"), order.read_attribute("tm_p_pa#{i}"), order.read_attribute("tm_u_pa#{i}"), "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.read_attribute("timi_m_p_a_#{i}"), "no"]
-            index +=1
+            index +=10
           end
         end
         #profil panw
         for i in 1..3
           if (order.read_attribute("pp#{i}_code") != nil)
-            csv << ["ARTPOS", main_index, main_index + "." + index.to_s, order.read_attribute("profil_panw_arithmos_#{i}"), "m", order.read_attribute("pp#{i}_code"), order.color_profil_p, 
+            @prp_color = Color.where(:name => order.color_profil_p).first
+            csv << ["ARTPOS", main_index, index.to_s, order.read_attribute("profil_panw_arithmos_#{i}"), "m", order.read_attribute("pp#{i}_code"), @prp_color.sungate_both, 
               order.read_attribute("profil_panw_#{i}"), order.read_attribute("tm_p_pp#{i}"), order.read_attribute("tm_u_pp#{i}"), "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.read_attribute("timi_m_p_p_#{i}"), "no"]
-            index +=1
+            index +=10
           end
         end
         #profil katw
         for i in 1..3
           if (order.read_attribute("pk#{i}_code") != nil)
-            csv << ["ARTPOS", main_index, main_index + "." + index.to_s, order.read_attribute("profil_katw_arithmos_#{i}"), "m", order.read_attribute("pk#{i}_code"), order.color_profil_k, 
+            @prk_color = Color.where(:name => order.color_profil_k).first
+            csv << ["ARTPOS", main_index, index.to_s, order.read_attribute("profil_katw_arithmos_#{i}"), "m", order.read_attribute("pk#{i}_code"), @prk_color.sungate_both, 
               order.read_attribute("profil_katw_#{i}"), order.read_attribute("tm_p_pk#{i}"), order.read_attribute("tm_u_pk#{i}"), "<Discount/Surcharge%>", "<Discount/Surcharge per unit>", order.read_attribute("timi_m_p_k_#{i}"), "no"]
-            index +=1
+            index +=10
           end
         end
         
