b46f851b8947c72e478c6f994cba2dd1216ecc28
olympiafili <olympia.filippa@gmail.com>
2017-04-24 22:03:56 +0300

    etc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { 
-    #host: "178.62.162.49:3000"
     host: "46.101.236.245:8850"
 
    }
@@ -94,8 +93,8 @@ Rails.application.configure do
     domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "olympia.filippa@gmail.com", # this should be a real gmail id
-    password: "abcdef01077!" # this should be real password of your gmail id
+    user_name: "serafimalbatec@gmail.com", # this should be a real gmail id
+    password: "" # this should be real password of your gmail id
   }
 
 end
